03f9a7995b07433d28e6923e633df36ad98bd54c
tiffanyhudson <tiffyhudson35@gmail.com>
2018-10-23 16:35:26 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -3,6 +3,14 @@ window.onload= function (){
 	boundary1.onmouseover = function() {
 		this.setAttribute("class", "boundary youlose");
 	}
-	
-
+	let boundary = document.querySelectorAll(".boundary");
+	let tracker = 0;
+	for (let a = 0; a < boundary.length; a++)
+	{
+		boundary[a].onmouseover = function(){
+			for (let b = 0; b< boundary.length-1;b++){
+				boundary[b].setAttribute("class","boundary youlose");
+			}
+		}
+	}
 }
