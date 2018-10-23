6a834cf8379106e9725be5260bf3d3e736c6e395
tiffanyhudson <tiffyhudson35@gmail.com>
2018-10-23 18:46:10 -0500

    Exercise 4,5,6

--- maze.js
+++ maze.js
@@ -20,30 +20,31 @@ window.onload= function (){
 		if (tracker == 0)
 		{
 			document.getElementById("status").innerHTML = "You Win";
-		};let s = document.getElementById("start");
-		s.onclick = function(){
-			tracker=0;
-			document.getElementById("status").innerHTML = "Move your mouse over the \"S\" to begin.";
-			for (let a = 0; a < boundary.length-1;a++) {
-				boundary[a].setAttribute("class","boundary boundary");
-			};
-			let maze = document.getElementById("maze");
-			document.onmousemove = function (event) {
-				let x = event.clientX;
-				let y = event.clientY;
-				let l = maze.offsetLeft;
-				let r = maze.offsetRight;
-				let h = maze.offsetHeight;
-				let w = maze.offsetWidth;
-				let t = maze.offsetTop;
-				if ((x < l)||(x < r))
+		};
+	}
+	let s = document.getElementById("start");
+	s.onclick = function(){
+		tracker=0;
+		document.getElementById("status").innerHTML = "Move your mouse over the \"S\" to begin.";
+		for (let a = 0; a < boundary.length-1;a++) {
+			boundary[a].setAttribute("class","boundary boundary");
+		};
+		let maze = document.getElementById("maze");
+		document.onmousemove = function (event) {
+			let x = event.clientX;
+			let y = event.clientY;
+			let l = maze.offsetLeft;
+			let r = maze.offsetRight;
+			let h = maze.offsetHeight;
+			let w = maze.offsetWidth;
+			let t = maze.offsetTop;
+			if ((x < l)||(x < r))
+			{
+				for (let b = 0; b< boundary.length ; b++)
 				{
-					for (let b = 0; b< boundary.length ; b++)
-					{
-						boundary[b].setAttribute("class","boundary youlose");
-					}
-				};
-			};	
-		};	
+					boundary[b].setAttribute("class","boundary youlose");
+				}
+			}
+		}
 	}
 }
